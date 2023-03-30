a05324714fd3c4d1e793addcf5c18efbbe556fb2
mahess14 <gmksvec14@gmail.com>
2023-03-30 10:46:06 +0530

    modification

--- src/test/java/testgithub1/Test.java
+++ src/test/java/testgithub1/Test.java
@@ -3,11 +3,11 @@ package testgithub1;
 public class Test {
 
 	public static void main(String[] args) {
-		int x=10;
-		int y=20;
+		int x=90;
+		int y=80;
 				int z=x+y;
 				
-				System.out.println(y);
+				System.out.println(z);
 	}
 
 }
